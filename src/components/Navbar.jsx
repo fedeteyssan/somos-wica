a5736e8c1307a2179e13eea6e4bb60de3e1f97e6
federico.teyssandier <fedeteyssan@gmail.com>
2021-10-11 20:32:19 -0300

    Agregando componente navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+
+import React from "react";
+import './Navbar.scss';
+import logo from '../images/Logo-Wica.jpg'
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <nav>
+                <a href="index.html">
+                    <img src={logo} alt="" className="brand-logo"/>
+                </a>
+                <ul className="nav-menu">
+                    <li>
+                        <a href="#Nosotros">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="#Productos">Productos</a>
+                    </li>
+                    <li>
+                        <a href="#Donde-comprar">Donde comprar</a>
+                    </li>
+                    <li>
+                        <a href="#Contacto">Contacto</a>
+                    </li>
+                </ul>
+                <div className="hamburger">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </nav>
+        </header>
+        
+    )
+}
+export default Navbar;
+
+
+
